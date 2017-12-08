0367d6ac8fbbcdb36aa366921236bac194d404f6
hajaja <dcs0417@163.com>
2017-12-08 23:03:21 +0800

    for server

--- dirbot/spiders/GeneralSpider.py
+++ dirbot/spiders/GeneralSpider.py
@@ -8,19 +8,22 @@ import traceback
 import re
 import time
 
+import Utils
+reload(Utils)
+
 class GeneralSpider(scrapy.Spider):
     name = 'GeneralSpider'
     start_urls = [
             'http://finance.sina.com.cn/',
-            #'http://finance.ifeng.com/',
-            #'http://finance.qq.com/',
+            'http://finance.ifeng.com/',
+            'http://finance.qq.com/',
             #'http://business.sohu.com/',
             #'http://www.cnstock.com/',
-            #'http://www.caijing.com.cn/',
-            #'http://www.jrj.com.cn/',
-            #'http://www.yicai.com/',
-            #'http://www.10jqka.com.cn/',
-            #'http://www.eastmoney.com/',
+            'http://www.caijing.com.cn/',
+            'http://www.jrj.com.cn/',
+            'http://www.yicai.com/',
+            'http://www.10jqka.com.cn/',
+            'http://www.eastmoney.com/',
             ]
     def parse(self, response):
         self.download_delay = 0.5
@@ -37,15 +40,20 @@ class GeneralSpider(scrapy.Spider):
             logging.log(logging.ERROR, 'Home page parse error')
             logging.exception(self.source)
             return
+
+        print 'TOTAL A: %d'%(len(listTaga))
         
         # crawl taga
         for taga in listTaga:
             full_url = taga.css('::attr(href)').extract_first().strip()
             if full_url.startswith('//'):
                 full_url = 'http:' + full_url
-                logging.log(logging.ERROR, full_url)
+                logging.log(logging.ERROR, 'double slash: %s'%full_url)
             title = taga.css('::text').extract_first()
-            yield scrapy.Request(full_url, callback=self.parse_page, meta={'title': title})
+            try:
+                yield scrapy.Request(full_url, callback=self.parse_page, meta={'title': title})
+            except:
+                logging.log(logging.ERROR, 'full_url error: %s'%full_url)
         
         '''
         # next page
@@ -67,111 +75,101 @@ class GeneralSpider(scrapy.Spider):
         
         try:
             if source is 'Sina':
-                #title = response.css('h1[id="artibodyTitle"]::text').extract_first()
                 dtCreated = response.css('span[class="time-source"]::text').extract_first()
                 if dtCreated is None:
                     # http://finance.sina.com.cn/zl/stock/20160616/082024822271.shtml
                     dtCreated = response.css('span[class="pub_date"]::text').extract_first()
-                elif dtCreated is None:
+                if dtCreated is None:
+                    # http://finance.sina.com.cn/zl/china/2017-12-08/zl-ifyppemf5823126.shtml
+                    dtCreated = response.css('span[id="pub_date"]::text').extract_first()
+                if dtCreated is None:
                     # http://finance.sina.com.cn/zl/stock/20160616/082024822271.shtml
                     dtCreated = response.css('span[class="time"]::text').extract_first()
-                elif dtCreated is None:
+                if dtCreated is None:
+                    # http://collection.sina.com.cn/cqyw/2017-12-05/doc-ifyphxwa7893955.shtml
+                    dtCreated = response.css('span[class="titer"]::text').extract_first()
+                if dtCreated is None:
                     # http://blog.sina.com.cn/u/1197890497
                     dtCreated = response.css('span[class="time SG_txtc"]::text').extract_first()[1:-1]
-                # replace nian yue ri in Chinese
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[id="artibody"]').css('::text').extract()
                 text = ' '.join(text)
                 text = re.sub('[\s\t\n][\s\t\n]+', '', text)
             elif source is 'Ifeng':
-                #title = response.css('h1[id="artical_topic"]::text').extract_first()
                 dtCreated = response.css('span[class="ss01"]::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[id="main_content"]').css('::text').extract()
                 text = ' '.join(text)
                 text = re.sub('[\s\t\n][\s\t\n]+', '', text)
             elif source is 'QQ':
                 dtCreated = response.css('span[class="a_time"]::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[id="Cnt-Main-Article-QQ"]').css('::text').extract()
                 text = ' '.join(text)
                 text = re.sub('[\s\t\n][\s\t\n]+', '', text)
             elif source is 'Sohu':
-                #title = response.css('h1[id="artical_topic"]::text').extract_first()
                 dtCreated = response.css('div[class="time"]::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[itemprop="article"]').css('::text').extract()
                 text = ' '.join(text)
                 text = re.sub('[\s\t\n][\s\t\n]+', '', text)
             elif source is 'CNStock':
-                #title = response.css('h1[class="title"]::text').extract_first()
                 if title is None:
                     title = response.css('h1::text').extract_first()
                 dtCreated = response.css('span[class="timer"]::text').extract_first()
                 if dtCreated is None:
                     dtCreated = response.css('span[class="time"]').extract_first()
                 if dtCreated is None:
+                    dtCreated = response.css('div[class="ll-time"]').extract_first()
+                if dtCreated is None:
+                    dtCreated = response.css('p[class="time"]').extract_first()
+                if dtCreated is None:
                     logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[id="qmt_content_div"]').css('p::text').extract()
                 text = ' '.join(text)
             elif source is 'Caijing':
-                #title = response.css('h1[id="cont_title"]::text').extract_first()
                 dtCreated = response.css('span[id="pubtime_baidu"]::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[id="the_content"]').css('::text').extract()
                 text = ' '.join(text)
                 pass
             elif source is 'JRJ':
-                #title = response.css('h1::text').extract_first()
                 dtCreated = response.css('p[class="inftop"]>span')[0].css('::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
+                dtCreated = Utils.parseDT(dtCreated)
+                text = response.css('div[class="texttit_m1"]').css('::text').extract()
+                text = ' '.join(text)
+                pass
+            elif source is 'Caijing':
+                dtCreated = response.css('span[id="pubtime_baidu"]::text').extract_first()
                 if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                    dtCreated = response.css('span[class="news_time"]::text').extract_first()
+                dtCreated = Utils.parseDT(dtCreated)
+                text = response.css('div[id="the_content"]').css('::text').extract()
+                text = ' '.join(text)
+                pass
+            elif source is 'JRJ':
+                dtCreated = response.css('p[class="inftop"]>span')[0].css('::text').extract_first()
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[class="texttit_m1"]').css('::text').extract()
                 text = ' '.join(text)
                 pass
             elif source is 'CBN':
                 dtCreated = response.css('h2>span')[1].css('::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
                 if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                    dtCreated = response.css('h3[class="f-fs24 f-tac"]>span')[1].css('::text').extract_first()
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[class="m-text"]').css('::text').extract()
                 text = ' '.join(text)
             elif source is 'THS':
-                #title = response.css('h1::text').extract_first()
                 dtCreated = response.css('span[id="pubtime_baidu"]::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[class="atc_content"]').css('::text').extract()
                 text = ' '.join(text)
                 pass
             elif source is 'EastMoney':
-                #title = response.css('h1::text').extract_first()
                 dtCreated = response.css('div[class="time"]>span')[0].css('::text').extract_first()
-                dtCreated = re.sub('[^\d:]+', ' ', dtCreated)   # replace non digit and colon
-                if dtCreated is None:
-                    logging.log(logging.DEBUG, response)
-                dtCreated = dateutil.parser.parse(dtCreated + '+8:00')
+                dtCreated = Utils.parseDT(dtCreated)
                 text = response.css('div[class="ContentBody"]').css('::text').extract()
                 text = ' '.join(text)
                 pass
@@ -180,9 +178,9 @@ class GeneralSpider(scrapy.Spider):
                 raise Exception('Unknown Source')
 
         except Exception:
-            logging.exception('Exception' + response.url)
+            logging.exception('Exception: %s'%response.url)
             title = response.meta['title']
-            dtCreated = ''
+            dtCreated = 'NULL'
             text = response.css('p::text').extract()
             text = ' '.join(text)
 
